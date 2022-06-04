9ca89dba4892c8d0aed52ee5944eb35e53cdf9ba
Juan Carlos <softjuancarlos@gmail.com>
2022-06-04 11:21:34 -0500

    Comentario de prueba 3

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <div class="max-w-3xl mx-auto">
         <h1 class="text-5xl mb-8">{{ $post->title }}</h1>
         <p class="leading-loose text-lg text-gray-700">
-            {{ $post->body }} <!-- Comentario de prueba 2 -->
+            {{ $post->body }} <!-- Comentario de prueba 3 -->
         </p>
     </div>
 @endsection
